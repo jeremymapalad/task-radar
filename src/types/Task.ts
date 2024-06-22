f7724d57d47b5e5b73daebd1ca3764726698597e
Jeremy Mapalad <jeremymapalad@gmail.com>
2024-06-23 01:19:49 +0800

    Export enum STATUS to the type Task for consistency

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -1,9 +1,10 @@
+import { STATUS } from "@/utils/constants";
 import firebase from "firebase/compat/app";
 
 export interface Task {
   id: string;
   label: string;
-  status: "To Do" | "In Progress" | "Completed";
+  status: STATUS;
   createdBy: string;
   createdAt: firebase.firestore.Timestamp;
 }
