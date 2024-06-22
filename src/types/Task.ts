ebc6ff152010313febcf47f56b002ffd7f2b983a
Jeremy Mapalad <jeremymapalad@gmail.com>
2024-06-23 01:38:13 +0800

    Add subTasks to the property

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -1,10 +1,12 @@
 import { STATUS } from "@/utils/constants";
 import firebase from "firebase/compat/app";
+import SubTaskType from "./SubTask";
 
 export interface Task {
   id: string;
   label: string;
   status: STATUS;
+  subTasks: SubTaskType[];
   createdBy: string;
   createdAt: firebase.firestore.Timestamp;
 }
